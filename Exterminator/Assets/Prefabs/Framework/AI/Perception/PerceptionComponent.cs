6a2a07ba52a32dfd0bd65ad9d6ec920bd7e6b939
dnzgo <dnzgzc.98@gmail.com>
2023-10-31 22:17:23 +0300

    Added Forgetting Routine

---
+++ Exterminator/Assets/Prefabs/Framework/AI/Perception/PerceptionComponent.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerceptionComponent : MonoBehaviour
+{
+    [SerializeField] SenseComponent[] senses;
+
+    private void Start()
+    {
+        foreach (SenseComponent sense in senses)
+        {
+            sense.onPerceptionUpdated += SenseUpdated;
+        }
+    }
+
+    private void SenseUpdated(PerceptionStimuli stimuli, bool successfulySensed)
+    {
+        
+    }
+}
--- Exterminator/Assets/Prefabs/Framework/AI/Perception/SenseComponent.cs
+++ Exterminator/Assets/Prefabs/Framework/AI/Perception/SenseComponent.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public abstract class SenseComponent : MonoBehaviour
 {
+    [SerializeField] float forgettingTime = 3f;
+
     static List<PerceptionStimuli> registeredStimulis = new List<PerceptionStimuli>();
     List<PerceptionStimuli> percievableStimulis = new List<PerceptionStimuli>();
+
+    Dictionary<PerceptionStimuli, Coroutine> forgettingRoutines = new Dictionary<PerceptionStimuli, Coroutine>();
+
+    public delegate void OnPerceptionUpdated(PerceptionStimuli stimuli, bool successfulySensed);
+
+    public event OnPerceptionUpdated onPerceptionUpdated;
 
     static public void RegisterStimuli(PerceptionStimuli stimuli)
     {
@@ -30,7 +38,16 @@ public abstract class SenseComponent : MonoBehaviour
                 if (!percievableStimulis.Contains(stimuli))
                 {
                     percievableStimulis.Add(stimuli);
-                    Debug.Log($"I just sensed {stimuli.gameObject}");
+                    if (forgettingRoutines.TryGetValue(stimuli, out Coroutine routine))
+                    {
+                        StopCoroutine(routine);
+                        forgettingRoutines.Remove(stimuli);
+                    }
+                    else
+                    {
+                        onPerceptionUpdated?.Invoke(stimuli, true);
+                        //Debug.Log($"I just sensed {stimuli.gameObject}");
+                    }
                 }
             }
             else
@@ -38,10 +55,18 @@ public abstract class SenseComponent : MonoBehaviour
                 if (percievableStimulis.Contains(stimuli))
                 {
                     percievableStimulis.Remove(stimuli);
-                    Debug.Log($"I lost track of {stimuli.gameObject}");
+                    forgettingRoutines.Add(stimuli, StartCoroutine(ForgetStimuli(stimuli)));
                 }
             }
         }
+    }
+
+    IEnumerator ForgetStimuli(PerceptionStimuli stimuli)
+    {
+        yield return new WaitForSeconds(forgettingTime);
+        forgettingRoutines.Remove(stimuli);
+        onPerceptionUpdated?.Invoke(stimuli, false);
+        //Debug.Log($"I lost track of {stimuli.gameObject}");
     }
 
     protected virtual void DrawDebug()
